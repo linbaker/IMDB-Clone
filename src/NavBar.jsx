a837ea662fe340bcf8fdbc5088fbea780ad03c57
lindsey baker and Liz Kelley <liz@>
2019-07-03 13:54:28 -0700

    style nav bar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,53 +5,35 @@ import Typography from '@material-ui/core/Typography';
 import { spacing } from '@material-ui/system';
 import NavTabs from './NavTabs';
 
-
 const NavBar = () => {
   var navBarStyles = {
-    display: 'grid',
-    gridTemplateColumns: '15 vw 85 vw',
-    color: 'black',
-    height: '10vh'
+    height: '100px'
   }
+
   var logoStyles = {
-    gridColumn: '1',
-    gridRow: '1 / 3',
-    height: '10vh',
-    marginTop: '5%',
+    height: '80px',
+    marginTop: '50px',
     border: '5px solid red'
   }
-  var topRowStyles = {
-    gridColumn: '2',
-    gridRow: '1/2',
-    height: '5vh',
-    border: '5px solid red'
-  }
+
   var botRowStyles = {
-    gridColumn: '2',
-    gridRowStart: '2',
-    gridRowEnd: '3',
-    height: '5vh',
     border: '5px solid red'
   }
-    return(
-        <div style={navBarStyles}>
-        <AppBar style={{ backgroundColor: '#000000' }} position="static">
-            <Toolbar>
-              <div style={logoStyles}>
-                <img height='50%' src={require("./assets/logo.png")} alt="IMDB Logo"/>
-              </div>
-              <div style={topRowStyles}>
-                <input mx="auto"
-                  type='text'
-                  id='search'
-                  placeholder='search'/>
-              </div>
-              <div style={botRowStyles}>
-                <NavTabs/>
-              </div>
-            </Toolbar>
-        </AppBar>
-        </div>
-    )
+
+  return(
+    <div style={navBarStyles}>
+      <AppBar style={{ backgroundColor: '#000000' }} position="static">
+        <Toolbar>
+          <div style={logoStyles}>
+            <img height='80px' src={require("./assets/logo.png")} alt="IMDB Logo"/>
+          </div>
+          <div style={botRowStyles}>
+            <NavTabs/>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
+
 export default NavBar;
--- src/NavTabs.jsx
+++ src/NavTabs.jsx
@@ -4,17 +4,36 @@ function NavTabs() {
 
   const divStyle={
     display: 'flex',
-    flexDirection: 'row'
-
+    flexDirection: 'row',
+    listStyle: 'none'
+  }
+  var buttonStyle={
+    backgroundColor: '#F5C518',
+    borderRadius: '10px',
+    border: 'solid black',
+    paddingLeft: '50px',
+    paddingRight: '50px'
+  }
+  var topRowStyles = {
+    border: '5px solid red',
+    width: '500px'
   }
   return(
-    <div style={divStyle}>
-      <h4>Watch Now For Free</h4>
-      <h4>Movies, TV & Showtimes</h4>
-      <h4>Celebs, Events & Photos</h4>
-      <h4>News & Community</h4>
-      <h4>Watchlist</h4>
-      <button>Sign in</button>
+    <div>
+      <div>
+        <input style={topRowStyles}
+          type='text'
+          id='search'
+          placeholder='search'/>
+      </div>
+      <ul className='navBar' style={divStyle}>
+        <li className='navItems'><h4>Watch Now For Free</h4></li>
+        <li className='navItems'><h4>Movies, TV & Showtimes</h4></li>
+        <li className='navItems'><h4>Celebs, Events & Photos</h4></li>
+        <li className='navItems'><h4>News & Community</h4></li>
+        <li className='navItems'><h4>Watchlist</h4></li>
+        <li className='navItems'><button style={buttonStyle}>Sign in</button></li>
+      </ul>
     </div>
   )
 }
